1234a92eff328e654349f3ae5d3fe4e3f701a80c
Liam Zdenek <liamzdenek@gmail.com>
2025-03-31 14:08:53 -0400

    feat: initial nx monorepo

---
+++ packages/ux/src/app/app.tsx
@@ -0,0 +1,12 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+
+export function App() {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
